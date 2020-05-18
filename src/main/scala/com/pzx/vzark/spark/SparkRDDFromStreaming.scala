55c8b1a678d3a7ef9bb043080df405c7376f15b7
pzx <2165985966@qq.com>
2020-05-18 09:30:47 +0800

    alter some data

--- src/main/scala/com/pzx/vzark/spark/SparkRDDFromStreaming.scala
+++ src/main/scala/com/pzx/vzark/spark/SparkRDDFromStreaming.scala
@@ -2,6 +2,9 @@ package com.pzx.vzark.spark
 
 object SparkRDDFromStreaming {
   def main(args: Array[String]): Unit = {
-    print("this is a text file")
+    val prices: List[Double] = List(5.0, 20.0, 9.95)
+    val ints: List[Int] = List(10, 2, 1)
+
+    println(prices.zip(ints))
   }
 }
