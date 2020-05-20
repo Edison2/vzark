939e68a48126a497a44d538f611766aae13f3424
pzx <2165985966@qq.com>
2020-05-20 18:30:23 +0800

    add code

--- analyse/session/src/main/scala/SessionStat.scala
+++ analyse/session/src/main/scala/SessionStat.scala
@@ -1,3 +1,19 @@
+import java.util.UUID
+
+import commons.conf.ConfigurationManager
+import commons.constant.Constants
+import net.sf.json.JSONObject
+
 object SessionStat {
 
+    def main(args: Array[String]): Unit = {
+        // 获取筛选条件
+        val jsonStr: String = ConfigurationManager.config.getString(Constants.TASK_PARAMS)
+        // 获取筛选条件对应的object
+        val taskParam = JSONObject.fromObject(jsonStr)
+
+        val taskUUID = UUID.randomUUID().toString
+
+
+    }
 }
